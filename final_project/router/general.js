eeb1b927b167eff2178473fe6f3f1def5847a32a
Bharath S <bharath.s@talentneuron.com>
2024-11-23 13:44:51 +0530

    task 5 is done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,18 +5,18 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
+public_users.post("/register", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', function (req, res) {
   return res.status(200).json(JSON.parse(JSON.stringify(books, null, 4)));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', function (req, res) {
   let isbn = req.params.isbn
   let data = null
   try {
@@ -25,10 +25,10 @@ public_users.get('/isbn/:isbn',function (req, res) {
     res.status(400).json({ error: error.toString() })
   }
   res.send(data)
- });
-  
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', function (req, res) {
   let author = req.params.author
   let data = []
   try {
@@ -44,7 +44,7 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', function (req, res) {
   let title = req.params.title
   let data = []
   try {
@@ -60,9 +60,15 @@ public_users.get('/title/:title',function (req, res) {
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn
+  let data = null
+  try {
+    data = books[isbn].reviews
+  } catch (error) {
+    res.status(400).json({ error: error.toString() })
+  }
+  res.status(200).json({ reviews: data })
 });
 
 module.exports.general = public_users;

5d8a304dd93ecc670ada245cc87ce891e2d1c600
Bharath S <bharath.s@talentneuron.com>
2024-11-23 15:48:56 +0530

    task 10,11,12,13 are  done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,5 +1,6 @@
 const express = require('express');
 let books = require("./booksdb.js");
+const  axios = require('axios');
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
@@ -25,6 +26,22 @@ public_users.get('/', function (req, res) {
   return res.status(200).json(JSON.parse(JSON.stringify(books, null, 4)));
 });
 
+public_users.get('/fetch-books-axios', async function (req, res) {
+  try {
+    const response = await axios.get('https://bharaths-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/'); // Replace 'URL_TO_GET_BOOKS' with the actual URL to fetch books
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ error: error.toString() });
+  }
+});
+
+public_users.get('/get-promissed-books', function (req, res) {
+  new Promise((resolve, reject) => {
+    resolve(books);
+  }).then(bookList => res.status(200).json(bookList))
+    .catch(error => res.status(500).json({ error }));
+});
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
   let isbn = req.params.isbn
@@ -35,6 +52,29 @@ public_users.get('/isbn/:isbn', function (req, res) {
     res.status(400).json({ error: error.toString() })
   }
   res.send(data)
+});
+
+public_users.get('/fetch-book-by-isbn/:isbn', async function (req, res) {
+  const isbn = req.params.isbn
+  try {
+    const response = await axios.get(`https://bharaths-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/isbn/${isbn}`); // Replace 'URL_TO_GET_BOOKS' with the actual URL to fetch books
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ error: error });
+  }
+});
+
+public_users.get("/get-promissed-book-by-isbn/:isbn", function (req, res) {
+  new Promise((resolve, reject) => {
+    resolve(books);
+  })
+    .then((bookList) => {
+      let data = null;
+      const isbn = req.params.isbn;
+      data = bookList[isbn];
+      res.status(200).json(data);
+    })
+    .catch((error) => res.status(500).json({ error }));
 });
 
 // Get book details based on author
@@ -53,6 +93,35 @@ public_users.get('/author/:author', function (req, res) {
   res.send(data)
 });
 
+
+public_users.get("/get-promissed-book-by-author/:author", function (req, res) {
+  new Promise((resolve, reject) => {
+    resolve(books);
+  })
+    .then((bookList) => {
+      let data = [];
+      const author = req.params.author;
+      Object.keys(bookList).forEach(key => {
+        if (bookList[key].author === author) {
+          data.push(bookList[key])
+        }
+      })
+      res.status(200).json(data);
+    })
+    .catch((error) => res.status(500).json({ error }));
+});
+
+public_users.get('/fetch-book-by-author/:author', async function (req, res) {
+  const author = req.params.author
+  try {
+    const response = await axios.get(`https://bharaths-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/author/${author}`); // Replace 'URL_TO_GET_BOOKS' with the actual URL to fetch books
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ error: error });
+  }
+});
+
+
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
   let title = req.params.title
@@ -69,6 +138,31 @@ public_users.get('/title/:title', function (req, res) {
   res.send(data)
 });
 
+public_users.get("/get-promissed-title/:title", function (req, res) {
+  new Promise((resolve, reject) => {
+      resolve(books);
+  }).then((bookList) => {
+          let data = [];
+          const title = req.params.title;
+          Object.keys(bookList).forEach(key => {
+              if (bookList[key].title === title) {
+                  data.push(bookList[key])
+              }
+          })
+      return res.status(200).json(data);
+  }).catch((error) => res.status(500).json({ error }));
+});
+
+public_users.get('/fetch-book-by-title/:title', async function (req, res) {
+  const title = req.params.title
+  try {
+      const response = await axios.get(`https://bharaths-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/title/${title}`); // Replace 'URL_TO_GET_BOOKS' with the actual URL to fetch books
+      return res.status(200).json(response.data);
+  } catch (error) {
+      return res.status(500).json({ error: error });
+  }
+});
+
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
   const isbn = req.params.isbn
@@ -80,5 +174,6 @@ public_users.get('/review/:isbn', function (req, res) {
   }
   res.status(200).json({ reviews: data })
 });
+
 
 module.exports.general = public_users;
